72d22c3ff8d34f63156744b50df5c5a84447346b
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2024-09-11 14:47:05 +0600

    Trying...

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
+// Problem 1
+
 function calculateTax(income, expenses) {
   if (
     income <= 0 ||
     expenses <= 0 ||
     income < expenses ||
-    income !== "number" ||
-    expenses !== "number"
+    typeof income !== "number" ||
+    typeof expenses !== "number"
   ) {
     return "Invalid Input";
   }
@@ -12,6 +14,8 @@ function calculateTax(income, expenses) {
   const tax = savedCost * 0.2;
   return tax;
 }
+
+// Problem 2
 
 function sendNotification(email) {
   if (email.indexOf("@") < 0) {
@@ -22,3 +26,50 @@ function sendNotification(email) {
   const message = username + " " + "sent you an email from" + " " + domainName;
   return message;
 }
+
+// Problem 3
+
+function checkDigitsInName(name) {
+  if (typeof name !== "string" || name === undefined) {
+    return "Invalid Input";
+  }
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  for (const num of numbers) {
+    for (const nameVal of name) {
+      if (num == nameVal) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+// Problem 4
+
+function calculateFinalScore(obj) {
+  if (typeof obj !== "object") {
+    return "Invalid Input";
+  }
+
+  let familyScore = 0;
+  if (obj.isFFamily === true) {
+    familyScore = 30;
+  }
+
+  const totalScore = obj.testScore + obj.schoolGrade + familyScore;
+  return totalScore >= 80 ? true : false;
+}
+
+// Problem 5
+
+function waitingTime(waitingTimes, serialNumber) {
+  if (serialNumber > waitingTimes.length) {
+    let totalTime = 0;
+    for (const time of waitingTimes) {
+      totalTime += time;
+    }
+    const averageTime = (totalTime) / waitingTime.length;
+    console.log(averageTime + (serialNumber - waitingTimes.length));
+  }
+}
+waitingTime([3, 5, 7, 11, 6], 10);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,6 +1,3 @@
-com"));
-console.log(sendNotification("farhan34@yahoo.com"));
-console.log(sendNotification("nadim.naem5@outlook.com"));
-console.log(sendNotification("zihad@gmail.com"));
-console.log(sendNotification("fahim234.hotmail.com"));
-console.log(sendNotification("sadia8icloud.com"));
+f(testScore) {
+
+  // }
